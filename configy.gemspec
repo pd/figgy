7002460814c25ee950c5e5031a507381f7c6f29d
Steven Degutis <steven.degutis@gmail.com>
2011-10-17 16:16:48 -0500

    Use aruba, heredoc_unindent gems for specs
    Signed-off-by: Kyle Hargraves <pd@krh.me>

--- configy.gemspec
+++ configy.gemspec
@@ -18,4 +18,6 @@ Gem::Specification.new do |s|
 
   s.add_development_dependency "rspec"
   s.add_development_dependency "simplecov"
+  s.add_development_dependency "aruba"
+  s.add_development_dependency "heredoc_unindent"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,11 @@ require 'simplecov'
 SimpleCov.start
 
 require 'rspec'
-
 require 'configy'
+require 'aruba/api'
+require 'heredoc_unindent'
+
+RSpec.configure do |c|
+  c.include Aruba::Api
+  c.after { FileUtils.rm_rf(current_dir) }
+end
